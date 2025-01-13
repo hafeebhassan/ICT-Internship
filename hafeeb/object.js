c5b2144a170cb5352120d5e8ce2c31c5c0724ac8
hafeebhassan <hhafeeb@gmail.com>
2025-01-13 16:09:40 +0530

    Add files via upload

---
+++ hafeeb/object.js
@@ -0,0 +1,16 @@
+let person = {
+    name: "john",
+    age: 30,
+    occupation:"engineer"
+}
+ 
+console.log(person.name)
+
+let people = [
+    { name: "John", age: 30, occupation: "engineer" },
+    { name: "Jane", age: 25, occupation: "Teacher" },
+    { name: "Bob", age: 40, occupation: "Doctor" } 
+]
+people[1].name = "Rahul";
+console.log(people[1]);
+
---
+++ hafeeb/sample.js
@@ -0,0 +1,10 @@
+var temp = 30;
+if (temp <=20) {
+    console.log("it is a cold day");
+}
+else if (temp > 20&&temp<=30) {
+    console.log("it is a normal weather");
+}
+else {
+    console.log("it is a hot day");
+}
