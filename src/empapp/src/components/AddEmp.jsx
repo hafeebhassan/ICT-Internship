ab44cfa2eb4a2ef90ad1171fefd69d0e73b77529
hafeebhassan <hhafeeb@gmail.com>
2025-01-17 10:33:29 +0530

    Add files via upload

---
+++ src/empapp/src/components/AddEmp.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Button, Container, TextField}from '@mui/material'
+const AddEmp = () => {
+  return (
+    <div>
+        <br /><br /><br />
+      <Container maxWidth="sm" style={{ backgroundColor: "whitesmoke", borderRadius:"100px"}}>
+        <br /><br />
+        <h1>Application Form</h1>
+        <br /><br />
+        <TextField variant='outlined'label="Enter name"/>
+        <br /><br />
+        <TextField variant='outlined'label="Enter age"type='Number'/>
+        <br /><br />
+        <TextField variant='outlined'label="Enter Department"/>
+        <br /><br />
+        <TextField variant='outlined'label="Enter Salary"/>
+        <br /><br />
+        <Button variant='contained'style={{backgroundColor:"black"}}>Add Employee</Button>
+        <br /><br /><br />
+        </Container>
+    </div>
+  )
+}
+
+export default AddEmp
---
+++ src/empapp/src/components/DataTable.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+const DataTable = () => {
+  return (
+    <div>
+         <br /><br />
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Age</TableCell>
+            <TableCell>Department</TableCell>
+            <TableCell>Salary</TableCell>
+            
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            <TableRow>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
+            </TableRow>
+        </TableBody>
+        </Table>
+
+    </div>
+  )
+}
+
+export default DataTable
